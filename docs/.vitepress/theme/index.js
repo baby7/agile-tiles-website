408660685622d1485c206683877c512a296fe024
baby7 <1160084407@qq.com>
2025-10-17 16:24:55 +0800

    增加github角标

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,16 +2,21 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import GitHubCorner from './components/GitHubCorner.vue'
 
 /** @type {import('vitepress').Theme} */
 export default {
     extends: DefaultTheme,
     Layout: () => {
         return h(DefaultTheme.Layout, null, {
-            // https://vitepress.dev/guide/extending-default-theme#layout-slots
+            // 在 layout-top 插槽中添加 GitHub Corner
+            'layout-top': () => h(GitHubCorner, {
+                repoUrl: 'https://github.com/baby7/agile-tiles'
+            })
         })
     },
     enhanceApp({ app, router, siteData }) {
-        // ...
+        // 如果需要，也可以在这里注册为全局组件
+        // app.component('GitHubCorner', GitHubCorner)
     }
 }
